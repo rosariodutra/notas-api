22570883af6f3d58d98b0eb7fff31d75b9aa0962
rosariodutra <duttra.maria@gmail.com>
2026-05-14 21:11:58 -0300

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Nota(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(100), nullable=False)
-    conteudo = db.Column(db.Text, nullable=False)
-    privada = db.Column(db.Boolean, default=True)
