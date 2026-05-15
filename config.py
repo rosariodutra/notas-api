b653d1620a962f41cd1524b1ea3dd167daea7a05
rosariodutra <duttra.maria@gmail.com>
2026-05-14 21:11:47 -0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = 'sua_chave_secreta'
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "notas.db")}'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
